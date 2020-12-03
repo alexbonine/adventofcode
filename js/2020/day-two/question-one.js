796eccde5df735cf7325830b6e46e1aa505e323b
Alex Bonine <lemurdev@gmail.com>
2020-12-03 00:06:36 -0500

    add npm and mix

---
+++ js/2020/day-two/question-one.js
@@ -0,0 +1,38 @@
+const data = require('./data');
+
+const parsed = data.map((item) => {
+  const [_ignore, min, max, char, pw] = item.match(/(\d*)-(\d*) ([^:]*): (.*)/);
+
+  return { min, max, chars: char.split('.'), pw };
+});
+
+const countLetters = (word) =>
+  [...word].reduce((accum, char) => {
+    if (accum[char]) {
+      accum[char] += 1;
+    } else {
+      accum[char] = 1;
+    }
+
+    return accum;
+  }, {});
+
+const invalids = {};
+
+parsed.forEach(({ min, max, chars, pw }, index) => {
+  const letters = countLetters(pw);
+
+  for (let i = 0; i < chars.length; i += 1) {
+    const count = letters[chars[i]] || 0;
+
+    if (count < min || count > max) {
+      if (!invalids[index]) {
+        invalids[index] = { pw, issues: [] };
+      }
+
+      invalids[index].issues.push({ letter: chars[i], min, max, count });
+    }
+  }
+});
+
+console.log(data.length - Object.keys(invalids).length); // 460
---
+++ js/2020/day-two/question-two.js
@@ -0,0 +1,26 @@
+const data = require('./data');
+
+const parsed = data.map((item) => {
+  const [_ignore, first, second, char, pw] = item.match(
+    /(\d*)-(\d*) ([^:]*): (.*)/
+  );
+
+  return { req: [first, second], chars: char.split('.'), pw };
+});
+
+const invalids = {};
+
+parsed.forEach(({ req, chars, pw }, index) => {
+  const isFirstLetter = pw[req[0] - 1] === chars[0];
+  const isSecondLetter = pw[req[1] - 1] === (chars[1] || chars[0]);
+
+  if (
+    (isFirstLetter && isSecondLetter) ||
+    (!isFirstLetter && !isSecondLetter)
+  ) {
+    console.log(`invalid ${index}`);
+    invalids[index] = { pw, isFirstLetter, isSecondLetter };
+  }
+});
+
+console.log(data.length - Object.keys(invalids).length); // 251
